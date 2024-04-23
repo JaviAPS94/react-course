fe0d40c01f79f87267bc69ea1949ebf5d2151098
alex.pinaida <alex.pinaida@rappi.com>
2024-04-23 18:27:05 -0500

    feat: add cars project using redux and redux tool kit

---
+++ cars/src/index.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import { createRoot } from 'react-dom/client';
+import App from './App';
+import { Provider } from 'react-redux';
+import { store } from './store';
+import './styles.css';
+import 'bulma/css/bulma.css';
+
+const element = document.getElementById('root');
+const root = createRoot(element);
+root.render(
+    <Provider store={store}>
+        <App />
+    </Provider>
+);
